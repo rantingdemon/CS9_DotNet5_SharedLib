a2a2d76e5be082a5916ffc7eb4263f25af266dce
rantingdemon <60785263+rantingdemon@users.noreply.github.com>
2021-09-26 12:55:59 +0200

    first
    first

---
+++ SharedLibrary/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+
+namespace Packt.Shared
+{
+    public static class StringExtensions
+    {
+        public static bool isValidXmlTag(this string input)
+        {
+            return Regex.IsMatch(input,
+                @"^<(<[a-z]+)(^<+)*(?:>(.*)<\/\1>|\s+\/>)$");
+        }
+
+        public static bool isValidPassword(this string input)
+        {
+            return Regex.IsMatch(input, @"^[a-zA-Z0-9_-]{8,}$");
+        }
+
+        public static bool isValidHex(this string input)
+        {
+            return Regex.IsMatch(input, "^#?([a-fA-F0-9]{3}|[a-fA-F0-9]{6})$");
+        }
+    }
+}
